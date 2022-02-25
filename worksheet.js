95ebd09af23dec6f64aa4ef50ef7496be0232900
John Parks <jcparks1984@gmail.com>
2022-02-25 14:50:53 -0500

    Completed Random Number Problem

--- worksheet.js
+++ worksheet.js
@@ -6,15 +6,30 @@
 // let colorInput = prompt('What is your favorite color?')
 // console.log("I've never seen a", animalInput, colorInput,'!')
 
-let timeOfDay = 1800
-let firstMeal = ''
-    if(timeOfDay < 1200){
-        console.log('breakfast')
-    }
-    else if(timeOfDay > 1700){
-        console.log('dinner')
-    }
-    else {
-        console.log('lunch')
-    }
+// let timeOfDay = 1800
+// let firstMeal = ''
+//     if(timeOfDay < 1200){
+//         console.log('breakfast')
+//     }
+//     else if(timeOfDay > 1700){
+//         console.log('dinner')
+//     }
+//     else {
+//         console.log('lunch')
+//     }
     
+
+// let randomNumber = Math.floor(Math.random() * 10);
+// if(randomNumber < 3){
+//     console.log('beatles')
+// }
+// else if(randomNumber > 2, randomNumber < 6){
+//     console.log('stones')
+// }
+// else if(randomNumber > 5, randomNumber < 9){
+//     console.log('floyd')
+// }
+// else{
+//     console.log('hendrix')
+// }
+
