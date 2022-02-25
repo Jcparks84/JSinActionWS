b03926597c11f8b1ef08f984fae4ed55b6e6ad90
John Parks <jcparks1984@gmail.com>
2022-02-25 14:35:16 -0500

    Completed First And Second Worksheet Problems.

--- worksheet.js
+++ worksheet.js
@@ -1,3 +1,20 @@
 // let dayOfWeek = 'monday'
-let dayOfWeek = 'friday'
-console.log(dayOfWeek)
+// let dayOfWeek = 'friday'
+// console.log("I can't wait for", dayOfWeek)
+
+// let animalInput = prompt('what is your favorite animal?')
+// let colorInput = prompt('What is your favorite color?')
+// console.log("I've never seen a", animalInput, colorInput,'!')
+
+let timeOfDay = 1800
+let firstMeal = ''
+    if(timeOfDay < 1200){
+        console.log('breakfast')
+    }
+    else if(timeOfDay > 1700){
+        console.log('dinner')
+    }
+    else {
+        console.log('lunch')
+    }
+    
